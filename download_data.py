a0828b40599e580e4950dc9590ba8c290aafb327
Stéphane Dumas <stephane.dumas.pro@gmail.com>
2024-02-28 18:20:53 +0100

    decrypt in download_data.py

--- download_data.py
+++ download_data.py
@@ -1,8 +1,7 @@
-import shutil
-import os
-from os.path import join
+# import shutil
 import gdown
-
+import os
+import subprocess
 def data_fetch():
     """
 
@@ -16,5 +15,8 @@ def data_fetch():
     
     url = "https://drive.google.com/drive/folders/1rwiMe6sSarsf51fMp_Rh_hgeTOI5WzVn"
     gdown.download_folder(url)
-
+    os.chdir('picture_reconstruction_dataset')
+    subprocess.call(['tar', '-zxf', 'Train.tgz'])
+    subprocess.call(['tar', '-zxf', 'Test.tgz'])
+    os.chdir('..')
     return
