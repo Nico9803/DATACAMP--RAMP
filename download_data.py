5ba7c19cec43b361ce88678038056b9a6da98bee
Stéphane Dumas <stephane.dumas.pro@gmail.com>
2024-03-03 10:02:01 +0100

    clean

--- download_data.py
+++ download_data.py
@@ -9,13 +9,12 @@ CHALLENGE_NAME = 'picture_reconstruction'
 
 def data_load():
     print("Data loading")
+
     # url = "https://drive.google.com/drive/folders/1TqEZjRlDm14QtC1cxFRmqGtzXmO6HBTP" # resolution 320x320 for y (64x64 for x)
     url = "https://drive.google.com/drive/folders/1Ih5l6HYhwsBK1GYJIyAPI-qIQfz15t2B" # resolution 128x128 for y (64x64 for x)
     gdown.download_folder(url)
 
-    # os.chdir('data')
     subprocess.call(['tar', '-zxf', 'data/data.tgz'])
-    # os.chdir('..')
     
     print("Unzip completed")
     return
